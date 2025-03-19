24502ae26586c79620e0a9cea0728e64e62591d3
Dustin Yang <srdpt3@gmail.com>
2025-03-18 20:19:56 -0400

    auth using stackserverapp

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import { UserButton } from "@stackframe/stack";
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <UserButton />
     </div>
   );
 }
